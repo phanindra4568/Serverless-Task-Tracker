7b36465c32af4ca2fc3393809dad1e498e0775c3
phanindra4568 <137140900+phanindra4568@users.noreply.github.com>
2025-12-04 13:19:42 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const API_URL = "https://YOUR_API_ID.execute-api.YOUR_REGION.amazonaws.com/prod";
+const COGNITO_DOMAIN = "https://YOUR_DOMAIN.auth.YOUR_REGION.amazoncognito.com";
+const CLIENT_ID = "YOUR_CLIENT_ID";
+const REDIRECT_URI = "http://YOUR-S3-WEBSITE-URL";
